5b4f7de0e29aa2b86ca79bbb246afc320df0247c
AND <a@and.sa>
2025-05-26 23:22:47 +0000

    added prettier and html formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
     formatters_by_ft = {
       ruby = { formatter },
       eruby = { "erb_format" },
+      html = { "prettier" }
     },
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
-    opts.ensure_installed = { "erb-formatter", "erb-lint" }
+    opts.ensure_installed = { "erb-formatter", "erb-lint", "prettier" }
     table.insert(opts.ensure_installed, "js-debug-adapter")
   end,
 }
