3f866e117ef8cf8f3b22752d78f97be729dbdb82
Abdullah Dahmash <a@and.sa>
2025-06-28 11:39:16 +0300

    merge masons conf files

--- lua/plugins/mason-org.lua
+++ lua/plugins/mason-org.lua
@@ -3,7 +3,24 @@ return {
   optional = true,
   opts = function(_, opts)
     opts.ensure_installed = opts.ensure_installed or {}
-    vim.list_extend(opts.ensure_installed, { "codelldb" })
+    vim.list_extend(
+      opts.ensure_installed,
+      {
+        "erb-formatter",
+        "erb-lint",
+        "prettier",
+        "cmakelang",
+        "cmakelint",
+        "goimports",
+        "gofumpt",
+        "gomodifytags",
+        "impl",
+        "delve",
+        "templ",
+        "codelldb",
+      }
+    )
+    table.insert(opts.ensure_installed, "js-debug-adapter")
     if diagnostics == "bacon-ls" then
       vim.list_extend(opts.ensure_installed, { "bacon" })
     end
